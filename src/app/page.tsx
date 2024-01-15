6a68e0bf96abe835eb1f5bcfe472b32608f7cfa7
Utku BAYRAKTAROĞLU <bayraktarogluutku@gmail.com>
2024-01-15 03:39:55 +0300

    signIn / signOut test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import { Button } from "@nextui-org/react";
-
+import * as actions from "@/actions";
 export default function Home() {
   return (
     <div>
-      {" "}
-      <Button> Click me!</Button>
+      <form action={actions.singIn}>
+        <Button type="submit"> Sign In!</Button>
+      </form>
+
+      <form action={actions.singOut}>
+        <Button type="submit"> Sign Out!</Button>
+      </form>
     </div>
   );
 }
