537fbe412e14b3287c214dc1e55396bad19f9723
Utku BAYRAKTAROĞLU <bayraktarogluutku@gmail.com>
2024-01-16 18:39:20 +0300

    add  src/paths.ts
    Path Helper Implementation

---
+++ src/paths.ts
@@ -0,0 +1,19 @@
+const paths = {
+  home() {
+    return "/";
+  },
+
+  topicShow(topicSlug: string) {
+    return `/topics/${topicSlug}`;
+  },
+
+  postCreate(topicSlug: string) {
+    return `/topics/${topicSlug}/post/new`;
+  },
+
+  postShow(topicSlug: string, postId: string) {
+    return `/topics/${topicSlug}/post/${postId}`;
+  },
+};
+
+export default paths;
