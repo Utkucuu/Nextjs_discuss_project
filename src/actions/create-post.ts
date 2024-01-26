abda82c7163b08c40fb828bc9424e56634e2bb78
Utku BAYRAKTAROĞLU <bayraktarogluutku@gmail.com>
2024-01-26 03:07:45 +0300

    Creating the Record

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -53,8 +53,28 @@ export async function createPost(
       },
     };
   }
-  return {
-    errors: {},
-  };
+  let post: Post;
+  try {
+    post = await db.post.create({
+      data: {
+        title: result.data.title,
+        content: result.data.content,
+        userId: session.user.id,
+        topicId: topic.id,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: { _form: [err.message] },
+      };
+    } else {
+      return {
+        errors: { _form: ["failed to create post"] },
+      };
+    }
+  }
+  revalidatePath(paths.topicShow(slug));
+  redirect(paths.postShow(slug, post.id));
 }
 // TODO: revalidate the topic show page
